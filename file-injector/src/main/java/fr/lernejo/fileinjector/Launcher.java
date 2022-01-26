ddba9fe9eadc5be7b7557aca9722d750e595f85a
thomas-mrx <thomas.moreaux@icloud.com>
2022-01-26 16:41:40 +0100

    Add json data injector

--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,54 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
+            if (args.length == 1) {
+                try {
+                    ObjectMapper mapper = new ObjectMapper();
+                    List<Map<String, Object>> map = mapper.readValue(Paths.get(args[0]).toFile(), new TypeReference<>() {});
 
-            System.out.println("Hello after starting Spring");
+                    RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+                    rabbitTemplate.setRoutingKey("game_info");
+
+                    for (Map<?, ?> el : map) {
+                        try {
+                            String orderJson = new ObjectMapper().writeValueAsString(el);
+                            Message message = MessageBuilder
+                                .withBody(orderJson.getBytes())
+                                .setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                                .setHeader("game_id", el.get("id"))
+                                .build();
+                            rabbitTemplate.convertAndSend(message);
+                        } catch (JsonProcessingException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 }
--- search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -26,14 +26,16 @@ public class GameInfoListener {
     public void onMessage(Message message) throws IOException {
         Map<String, Object> headers = message.getMessageProperties().getHeaders();
         String type = message.getMessageProperties().getContentType();
-        String id = headers.get("game_id").toString();
-        if (id == null || type == null) {
+        Object id = headers.get("game_id");
+        System.out.println(message);
+        if (id == null || !type.equals("application/json")) {
             return;
         }
+        int game_id = (int) id;
         /*DeleteIndexRequest requests = new DeleteIndexRequest("games");
         client.indices().delete(requests, RequestOptions.DEFAULT);*/
 
-        IndexRequest request = new IndexRequest("games").id(id);
+        IndexRequest request = new IndexRequest("games").id(String.valueOf(game_id));
         request.source(message.getBody(), XContentType.JSON);
         IndexResponse response = client.index(request, RequestOptions.DEFAULT);
         System.out.println(response);
